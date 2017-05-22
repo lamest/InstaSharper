de89a9d9060e80c8475d5eb0ae80e8d8907676d2
Садовский Антон Дмитриевич <mn0016@coins-business.local>
2017-05-22 17:00:51 +0700

    Added pagination objects

---
+++ InstaSharper/Classes/IPaginationData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaSharper.Classes
+{
+    public interface IPaginationData
+    {
+        Pagination Pagination { get; }
+    }
+}
---
+++ InstaSharper/Classes/Pagination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstaSharper.Classes
+{
+    public class Pagination
+    {
+        public string NextId { get; }
+        public Pagination(string nextId)
+        {
+            NextId = nextId;
+        }
+        
+    }
+}
